32be5fb6e6454177bd6258e3666d8256b60ec3bc
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-04-06 18:22:25 +0900

    3カードをツーペアと判断しないようにした

--- src/poker/Player.java
+++ src/poker/Player.java
@@ -1,8 +1,10 @@
 package poker;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Player {
@@ -18,10 +20,17 @@ public class Player {
     }
 
     public boolean isTwoPair() {
-        Set<Integer> numberSet = new HashSet<Integer>();
+        Map<Integer, Integer> numberMap = new HashMap<Integer, Integer>();
         for (Card card : cards) {
-            numberSet.add(card.number);
+            if (!numberMap.containsKey(card.number)) numberMap.put(card.number, 1);
+            else {
+                numberMap.put(card.number, numberMap.get(card.number) +1);
+            }
         }
-        return numberSet.size() == 3;
+        if (numberMap.size() != 3) return false;
+        for (Integer count : numberMap.values()) {
+            if (count == 3) return false;
+        }
+        return true;
     }
 }
